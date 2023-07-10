d616239048093939d5e5ab90fb85622a32e26e5d
Dev26x <dev26x@outlook.com>
2023-07-10 14:22:45 +0000

    product view edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-   <footer className='bg-primary py-12 my-12'>
+   <footer className='bg-primary py-12'>
     <div className='container mx-auto'>
       <p className='text-white text-center'>
         Copyright &copy; Shop-E by <a target='_blank' rel="noreferrer" href='https://twitter.com/'>
